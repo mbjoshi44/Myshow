2ade2877010fe57212c5880daa372307627513ec
mbjoshi44 <119665891+mbjoshi44@users.noreply.github.com>
2023-05-12 15:05:54 +0530

    done22

--- src/components/lazyloadimage/img.jsx
+++ src/components/lazyloadimage/img.jsx
@@ -3,15 +3,14 @@ import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
-const Img = ({ src, className }) => {
+function Img({ image, className }) {
     return (
         <LazyLoadImage
             className={className || ""}
             alt=""
             effect="blur"
-            src={src}
-        />
+            src={image.src} />
     );
-};
+}
 
 export default Img;
